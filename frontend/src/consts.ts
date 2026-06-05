6154403a0749a7e4f092fc8f74239ec1bd383720
ChristianReinsberg <christianreinsberg@web.de>
2026-06-05 23:13:08 +0200

    add missing const

--- frontend/src/consts.ts
+++ frontend/src/consts.ts
@@ -1,1 +1,10 @@
+import { MediaType } from "./types";
+
 export const img_base = 'https://media.themoviedb.org/t/p/w342';
+
+export const to: Record<MediaType, string> = {
+    'Character': 'character',
+    'Movie': 'movie',
+    'Short': 'shorts',
+    'TV': 'series',
+}
